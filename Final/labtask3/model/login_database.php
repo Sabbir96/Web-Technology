49f2388dc7ce6b98434a90b955512d3404014868
Sabbir Islam <40674290+Sabbir96@users.noreply.github.com>
2020-12-16 23:57:21 +0600

    initial commit

---
+++ Final/labtask3/model/login_database.php
@@ -0,0 +1,32 @@
+<?php
+
+    function login($id, $password){
+        $servername = "localhost";
+        $username = "root";
+        $pass = "";
+        $dbname = "UserDatabase";
+
+     
+        $conn = new mysqli($servername, $username, $pass, $dbname);
+        
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+
+
+        $sql = "SELECT id FROM userinfo WHERE id='".$id."'AND password= '".$password."' ";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+        
+            $conn->close();
+            return true;
+        } 
+        else {
+            $conn->close();
+            echo "0 results";
+            return false; 
+        }
+    }
+	
+?>
